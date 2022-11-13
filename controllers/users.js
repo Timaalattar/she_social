ab37b3520c610069d7b225e18929f006a4b2fb04
Timaalattar <fatima.alattar@gmail.com>
2022-11-13 08:50:11 +0300

    updated the user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,52 @@
 const { json } = require('express')
+const User = require('../models/User')
+
+async function user_create_post(req,res) {
+    try {
+    } catch (err) {
+        res.json(err)
+    }
+}
+
+async function user_login_post(req,res) {
+    try {
+    } catch (err) {
+        res.json(err)
+    }
+}
+
+async function user_login_post(req,res) {
+    try {
+    } catch (err) {
+        res.json(err)
+    }
+}
+
+async function user_details_get(req,res) {
+    try {
+    } catch (err) {
+        res.json(err)
+    }
+}
+
+async function user_update_put(req,res) {
+    try {
+    } catch (err) {
+        res.json(err)
+    }
+}
+
+async function user_delete(req,res) {
+    try {
+    } catch (err) {
+        res.json(err)
+    }
+}
+
+module.exports = {
+    user_create_post,
+    user_login_post,
+    user_details_get,
+    user_update_put,
+    user_delete,
+}
