2d410831ab0aebff93101a8dc59adfb2c5e1a946
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-11-14 16:11:32 +0300

    signin component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,90 @@
 import './App.css';
+// import NavBar from './components/NavBar/NavBar'
+// import HomePage from './components/HomePage/HomePage'
+// import EditPage from './components/EditPage/EditPage'
+// import ProfilePage from './components/ProfilePage/ProfilePage';
+// import DetailPage from './components/DetailPage/DetailPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Signup from './components/user/Signup'
+import Signin from './Components/User/Signin'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
+  const [User, setUser] = useState({});
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+
+    if(token != null){
+      let user = jwt_decode(token);
+
+      if(user){
+        setIsAuth(true);
+        setUser(user)
+      }
+      else if(!user){
+        localStorage.removeItem("token");
+        setIsAuth(false);
+      }
+    }
+  }, [])
+  
+
+  const registerHandler = (user) => {
+    axios.post("http://localhost:4000/users", user)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  }
+
+  const loginHandler = (cred) => {
+    axios.post("http://localhost:4000/auth/signin", cred)
+    .then(res => {
+      console.log(res.data.token)
+
+      // Store the token in Local Storage.
+      if(res.data.token != null){
+        localStorage.setItem("token", res.data.token);
+        let user = jwt_decode(res.data.token);
+        setIsAuth(true);
+        setUser(user);
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const onLogoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setIsAuth(false);
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <NavBar onLogoutHandler={onLogoutHandler} isAuth={isAuth} user={user}/>
+      <div className="App">
+        <Routes>
+          {/* <Route path='/home' element={isAuth ? <HomePage /> : <Signin login={loginHandler}></Signin>} />
+          <Route path='/edit/:userId' element={<EditPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/detail/:tweetId' element={<DetailPage />} />
+          <Route path='*' element={<HomePage />} />
+
+          <Route path="/signup" element={<Signup register={registerHandler}></Signup>}></Route> */}
+          <Route path="/signin" element={isAuth ? <HomePage></HomePage> : <Signin login={loginHandler}></Signin>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
