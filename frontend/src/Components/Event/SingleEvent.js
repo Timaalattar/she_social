74727cf4517abc88f12794a847652b8a99f9ba58
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-11-15 12:22:07 +0300

    workin in Event Detail

--- frontend/src/Components/Event/SingleEvent.js
+++ frontend/src/Components/Event/SingleEvent.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 function SingleEvent() {
-  return (
-    <div>SingleEvent</div>
-  )
+
+const [event, setEvent] = useState({})
+
+const params = useParams()    
+
+console.log(params)
+
+useEffect(() => {
+  axios.get(`http://localhost:4000/events/'${params.eventId}`)
+  .then(res => setEvent(res.data))
+}, [])
+return (
+  <div>
+      {event ? 
+      <div>
+        
+          <p>{event.Date}</p>
+          <p>{tweet.Time}</p>
+          <p>{event.Locate}</p>
+          <p>{event.Category}</p>
+          <p>{event.Description}</p>
+
+      </div>    
+  : null} 
+  </div>
+)
 }
 
 export default SingleEvent
