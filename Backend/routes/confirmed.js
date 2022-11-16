b28e19fa1bbcdf607dca3b4b43e67fb184f73d0a
Timaalattar <fatima.alattar@gmail.com>
2022-11-16 16:50:21 +0300

    completed the backend for the confirmed

--- Backend/routes/confirmed.js
+++ Backend/routes/confirmed.js
@@ -6,12 +6,12 @@ const confirmedController = require('../controllers/confirmed')
 router.post('/events/:eventId/confirmed', confirmedController.confirmed_create_post)
 
 //View list of events you confirmed to
-router.get('/user/:userId/confirmed', confirmedController.confirmed_details_get)
+router.get('/users/:userId/confirmed', confirmedController.confirmed_details_get)
 
 //delete detail confrimaton
-router.delete('/user/:userId/confirmed', confirmedController.confirmed_delete)
+router.delete('/users/confirmed/:confirmedId', confirmedController.confirmed_delete)
 
 //list all event attendees
-router.get('/users/events/:eventId', confirmedController.confirm_search_get)
+router.get('/events/:eventId/confirmed', confirmedController.confirm_search_get)
 
 module.exports = router
