1306232684c8790547c18e18ab8b46fdc438d538
Timaalattar <fatima.alattar@gmail.com>
2022-11-17 09:51:02 +0300

    fixed the confirmed event and the edit profile page section

--- frontend/src/Components/ProfilePage/ProfilePage.js
+++ frontend/src/Components/ProfilePage/ProfilePage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './ProfilePage.css'
+import { Link } from 'react-router-dom'
 
-function ProfilePage() {
+
+function ProfilePage(props) {
+  // console.log(props)
   return (
-    <div>ProfilePage</div>
+    <>
+      <div>
+      <ul className="profilenavbar">
+      <li><Link to={`/profile/${props.user.id}/Edit`}>Edit Profile</Link></li>
+      <li><Link to="/profile/createdevents">Hosted Events</Link></li>
+      <li><Link to="/profile/confirmedevents">Confrimed Events</Link></li>
+      </ul>
+    </div>
+    </>
   )
 }
 
-export default ProfilePage
+export default ProfilePage;
